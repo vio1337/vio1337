e0a33af7dd5d129437d24e0a90d9a193a1784168
violet moon <violet.wmoon@gmail.com>
2018-12-17 02:13:31 -0600

    added needed dependencies and rendered app to browser

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import './styles/App.css'
+
+interface Props {}
+interface State {}
+
+class App extends Component<Props, State> {
+
+  render() {  
+    return (
+      <div className="App">
+        <h1>Hi, imma portfolio</h1>
+      </div>
+    )
+
+  }
+}
+
+export default App

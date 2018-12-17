3f03ccdf33ff9738ac3798a69670c030693226ae
violet moon <violet.wmoon@gmail.com>
2018-12-17 03:32:20 -0600

    got div fragments to jump sections

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,49 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
+import { withStyles, createStyles, Theme } from '@material-ui/core/styles'
 import './styles/App.css'
 
-interface Props {}
+interface Props {classes: any}
 interface State {}
+
+const styles = createStyles({
+  container: {
+    minHeight: '100vh',
+  }
+})
 
 class App extends Component<Props, State> {
 
-  render() {  
+  handleClick = () => {
+    console.log('ive been clicked!')
+  }
+
+  render() { 
+    const {classes} = this.props
+
     return (
-      <div className="App">
-        <h1>Hi, imma portfolio</h1>
-      </div>
+      <Fragment>
+        <div className={classes.container}>
+          <h1>Hi, imma portfolio</h1>
+          <a href="#about">go to section 1</a>
+          <a href="#contact">got to section 2</a>
+        </div>
+
+        <div className={classes.container} id="about">
+          <div>
+            <h1>I'm the about section</h1>
+          </div>
+        </div>
+
+        <div className={classes.container} id="contact">
+          <div>
+            <h1>I'm the contact section</h1>
+          </div>
+        </div>
+
+      </Fragment>
     )
 
   }
 }
 
-export default App
+export default withStyles(styles)(App)
---
+++ src/components/About.tsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+interface Props {}
+interface State {}
+
+class About extends React<Props, State> {
+	render() {
+		return (
+		)
+	}
+}
+
+export default About
